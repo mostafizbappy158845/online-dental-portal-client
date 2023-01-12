5f7357595ac629e032613ceaa3b895e6af9df350
mostafizbappy <mostafizur15-8845@diu.edu.bd>
2023-01-12 14:55:33 +0600

    home>banner>hero-sec done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,25 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          primary: "#0FCFEC",
+
+          secondary: "#19d3ae",
+
+          accent: "#3a4256",
+
+          neutral: "#3d4451",
+
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
+
   theme: {
     extend: {},
   },
