25b7c87a17fe370e5047cffc71225dae5d3894f4
mostafizbappy <mostafizur15-8845@diu.edu.bd>
2023-01-15 13:56:56 +0600

    appointment option part done

--- src/pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -1,10 +1,28 @@
 import { format } from 'date-fns/esm';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AppointmentOption from './AppointmentOption';
 
 const AvailableAppointment = ({selectedDate}) => {
+    const [appointmentoptions, setAppointmentoptions] = useState([])
+
+    useEffect(() => {
+      fetch('appointmentOptions.json')
+      .then(res => res.json())
+      .then(data =>setAppointmentoptions(data))
+    }, [])
+    
     return (
         <section className='mt-16'>
             <p className='text-center text-secondary font-bold'>Available Appointments on : {format(selectedDate, 'PP')}</p>
+            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10'>
+                {
+                    appointmentoptions.map(option =><AppointmentOption
+                    key={option._id}
+                    appointmentOption = {option}
+
+                    ></AppointmentOption>)
+                }
+            </div>
         </section>
     );
 };
