75f1aeccefabab57191d0e315855315f9a566b4d
mostafizbappy <mostafizur15-8845@diu.edu.bd>
2023-01-17 11:28:44 +0600

    private-route,loading,redirect-user

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -5,6 +5,7 @@ import Dashboard from "../../pages/Dashboard/Dashboard/Dashboard";
 import Home from "../../pages/Home/Home/Home";
 import Login from "../../pages/Login/Login";
 import SignUp from "../../pages/SignUp/SignUp";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
 
 const router = createBrowserRouter([
     {
@@ -32,7 +33,7 @@ const router = createBrowserRouter([
     },
     {
         path: '/dashboard',
-        element: <Dashboard></Dashboard>
+        element: <PrivateRoute><Dashboard></Dashboard></PrivateRoute>
     }
 ])
 
--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -8,11 +8,14 @@ const auth = getAuth(app)
 const AuthProvider = ({children}) => {
 
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     const createUser = (email,password) =>{
+        setLoading(true);
         return createUserWithEmailAndPassword(auth,email,password);
     }
     const signIn = (email,password) =>{
+        setLoading(true);
         return signInWithEmailAndPassword(auth,email,password);
     }
 
@@ -21,6 +24,7 @@ const AuthProvider = ({children}) => {
     } 
 
     const logOut = () =>{
+        setLoading(true);
         return signOut(auth);
     }
 
@@ -28,6 +32,7 @@ const AuthProvider = ({children}) => {
       const unSubscribe = onAuthStateChanged(auth, currentUser =>{
         console.log('user observing');
         setUser(currentUser);
+        setLoading(false);
       });
 
       return() => unSubscribe();
@@ -39,7 +44,8 @@ const AuthProvider = ({children}) => {
         signIn,
         updateUser,
         logOut,
-        user
+        user,
+        loading
 
     }
     return (
